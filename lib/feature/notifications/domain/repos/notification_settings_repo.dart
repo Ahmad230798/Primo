c8b9d9f58242af10cdf4bc95ea7cb2c671c6b75b
MajdKhalifa <majdkhalifa321@gmail.com>
2026-07-06 17:26:34 +0300

    fixed location problems

---
+++ lib/feature/notifications/domain/repos/notification_settings_repo.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:primo/core/network/api_error_handler.dart';
+import 'package:primo/feature/notifications/data/models/notification_settings_model.dart';
+
+abstract class NotificationSettingsRepo {
+  Future<Either<Failure, NotificationSettingsModel>> getNotificationSettings();
+  Future<Either<Failure, NotificationSettingsModel>> updateNotificationSettings(NotificationSettingsModel settings);
+}
